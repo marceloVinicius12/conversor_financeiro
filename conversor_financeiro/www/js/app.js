8ea2c5771a6969340778879f4ccddd7e134e2542
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-06 20:19:43 -0300

    fazendo a conversao de dolar para real

--- conversor_financeiro/www/js/app.js
+++ conversor_financeiro/www/js/app.js
@@ -1,21 +1,47 @@
-function ConverterRealDolar (){
-let real = document.querySelector('#valorReais').value
-let dolar = 4.92
+ function ConverterRealDolar (){
+ let real = document.querySelector('#valorReais').value
+ let dolar = 4.92
+ let api = 'https://economia.awesomeapi.com.br/last/USD'
+ let somaPadrao = real / dolar;
+ 
+ 
+ $.ajax({
+     type:"GET", 
+     url: api,
+   success:(function(retorno){
+         dolar = retorno.USDBRL.ask
+       valorConvertido = real / dolar
+       document.querySelector('#valorDolares').value= valorConvertido.toFixed([2]);
+       document.querySelector('#valorReais').value = "";
+    
+   }),
+    beforeSend: (function (){
+        valorConvertido = real / dolar; //caso a api nao funcione o valor fixo do dolar é de 4.92
+        document.querySelector('#valorDolares').value= somaPadrao.toFixed([2]);
+        document.querySelector('#valorReais').value = "";
+    })  
+ })
+
+ }
+function inverterValores (){
+let dolarDigitado = document.querySelector('#valorDolares').value
 let api = 'https://economia.awesomeapi.com.br/last/USD'
-let somaPadrao = real / dolar
+let somaPadrao = dolarDigitado * 4.92
 $.ajax({
     type:"GET", 
     url: api,
     success:(function(retorno){
         dolar = retorno.USDBRL.ask
-        valorConvertido = real / dolar
-        document.querySelector('#valorDolares').innerHTML= valorConvertido.toFixed([2]) 
+        valorReal = dolarDigitado * dolar;
+        document.querySelector('#valorReais').value = valorReal;
+        document.querySelector('#valorDolares').value = ""
     }),
     beforeSend: (function (){
-        valorConvertido = real / dolar; //caso a api nao funcione o valor fixo do dolar é de 4.92
-        document.querySelector('#valorDolares').innerHTML= somaPadrao.toFixed([2])
-    })  
+        somaPadrao = dolarDigitado * 4.92 //caso a api nao funcione o valor fixo do dolar é de 4.92
+        document.querySelector('#valorDolares').value= somaPadrao.toFixed([2])
+        document.querySelector('#valorDolares').value = ""
+    }), 
 })
-document.querySelector('#texto').innerHTML = 'valor convertido com a contação do Dolar atual que é no valor  de  ' + dolar
 
 }
+
