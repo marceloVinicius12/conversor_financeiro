20dc57623d0a2d80fdc57a437df46e808a668ad5
marcelo vinicius <viniciusmarcello12@gmail.com>
2022-05-06 12:00:54 -0300

    adicionando recurso offline

--- conversor_financeiro/www/js/app.js
+++ conversor_financeiro/www/js/app.js
@@ -1,7 +1,8 @@
 function ConverterRealDolar (){
 let real = document.querySelector('#valorReais').value
 let dolar = 4.92
-let api = 'https://economia.awesomeapi.com.br/last/USD'
+let api = 'https://economia.awesomepi.com.br/last/USD'
+let somaPadrao = real / dolar
 $.ajax({
     type:"GET", 
     url: api,
@@ -11,7 +12,8 @@ $.ajax({
         document.querySelector('#valorDolares').innerHTML= valorConvertido.toFixed([2]) 
     }),
     beforeSend: (function (){
-        console.log("processando")
+        valorConvertido = real / dolar; //caso a api nao funcione o valor fixo do dolar é de 4.92
+        document.querySelector('#valorDolares').innerHTML= somaPadrao.toFixed([2])
     })  
 })
 document.querySelector('#texto').innerHTML = 'valor convertido com a contação do Dolar atual que é no valor  de  ' + dolar
